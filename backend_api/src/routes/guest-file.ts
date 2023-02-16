c46dfcda68e084248ad8d3ead5d9c720690b4e73
Rocky Essel <rockyessel76@gmail.com>
2023-02-16 17:25:51 +0000

    Delete backend_api directory

--- backend_api/src/routes/guest-file.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { GuestPost } from '../controllers/guest-file';
-import { upload } from '../utils/services';
-
-const router = express.Router();
-
-router.post('/guest', upload.single('file'), GuestPost);
-
-export = router;

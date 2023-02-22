b1d7eaa515025e5078abd2a79476191dd098d815
Rocky Essel <rockyessel76@gmail.com>
2023-02-22 15:28:24 +0000

    ijfsd

--- backend_api/src/index.ts
+++ backend_api/src/index.ts
@@ -9,27 +9,43 @@ import { connectDatabase } from './utils/configs/database';
 import fs from 'fs';
 import path from 'path';
 
-const private_key = fs.readFileSync('./private.key');
-const certificate = fs.readFileSync('./certificate.crt');
+// const key = fs.readFileSync('./private.key');
+// const cert = fs.readFileSync('./certificate.crt');
 
-const HTTPS_Credentials = {
-  key: private_key,
-  certificate,
-};
+// const cred = {
+//   key,
+//   cert,
+// };
 
 dotenv.config();
 
+const app: Express.Application = Express();
+
 // @desc Connecting to database
 connectDatabase();
+
+const SSLCertificate =
+  '/.well-known/pki-validation/C6A00B6CF036DEBB512DC0F162D7AC17.txt';
+const text = `49216D040068C8C945ACC6A57DE363AACA6C1837E82D0D25B6CA153151A676FD
+comodoca.com
+0e3ee3fdf3bf9cc`;
+fs.writeFile('./build/C6A00B6CF036DEBB512DC0F162D7AC17.txt', text, (error) => {
+  if (error) throw new Error('Failed to write');
+  console.log(`file created successfully`);
+});
+
+const fileName = 'C6A00B6CF036DEBB512DC0F162D7AC17.txt';
+const filePath = path.join(__dirname, fileName);
+
+app.get(SSLCertificate, async (request, response) => {
+  response.sendFile(filePath);
+});
 
 // @desc Routes Imports
 import GuestFile from './routes/guest-file';
 // @desc PORT
 const PORT = process.env.PORT || 7789;
 export const baseURL = `http://localhost:${PORT}`;
-
-const app: Express.Application = Express();
-const HTTPS_Server = https.createServer(HTTPS_Credentials, app);
 
 app.use(cors({ origin: '*' }));
 app.use(morgan('tiny'));
@@ -45,4 +61,5 @@ app.listen(PORT, () => {
   console.log(`Server is running on ${baseURL}`);
 });
 
-HTTPS_Server.listen(8443);
+// const HTTPS_Server = https.createServer(cred, app);
+// HTTPS_Server.listen(8443);

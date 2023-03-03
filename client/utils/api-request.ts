49c0bc5ff71d1ca8a52c5646b2cc794eee810f56
Rocky Essel <rockyessel76@gmail.com>
2023-03-03 22:28:33 +0000

    fetch

--- client/utils/api-request.ts
+++ client/utils/api-request.ts
@@ -6,9 +6,13 @@ import axios from 'axios';
 const API_URI = process.env.NEXT_PUBLIC_API_URI;
 // const API_URI = `https://localhost:8443/`;
 
-const ca =  await fetch('http://localhost:3000/52.4.183.221.chained+root.crt').then(res => res.arrayBuffer()).then(buf => Buffer.from(buf));
-const key = await  fetch('http://localhost:3000/private.key').then(res => res.arrayBuffer()).then(buf => Buffer.from(buf));
-const cert = await fetch('http://localhost:3000/certificate.crt').then(res => res.arrayBuffer()).then(buf => Buffer.from(buf));
+const ca_path = `${process.env.NODE_ENV === 'development' ? 'http://localhost:3000/52.4.183.221.chained+root.crt' : process.env.NODE_ENV === 'production' ? 'https://klaudbox.vercel.app/52.4.183.221.chained+root.crt' : '' }`
+const key_path = `${process.env.NODE_ENV === 'development' ? 'http://localhost:3000/private.key' : process.env.NODE_ENV === 'production' ? 'https://klaudbox.vercel.app/private.key' : '' }`
+const cer_path = `${process.env.NODE_ENV === 'development' ? 'http://localhost:3000/certificate.crt' : process.env.NODE_ENV === 'production' ? 'https://klaudbox.vercel.app/certificate.crt' : '' }`
+
+const ca =  await fetch(ca_path).then(res => res.arrayBuffer()).then(buf => Buffer.from(buf));
+const key = await  fetch(key_path).then(res => res.arrayBuffer()).then(buf => Buffer.from(buf));
+const cert = await fetch(cer_path).then(res => res.arrayBuffer()).then(buf => Buffer.from(buf));
 
 const agent = new https.Agent({
   ca: [ca],
